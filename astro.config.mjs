59ceb131220c9931efbe3c089f965c1a545765ae
Daniel Kim <danielkimyoyo@gmail.com>
2025-12-02 15:42:20 -0800

    made three sections

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    domains: ["i.imgur.com"],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
