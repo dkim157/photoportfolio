eec40d84f6eb686e303e835cbb85bf7c0eba9901
Daniel Kim <danielkimyoyo@gmail.com>
2025-12-03 16:20:32 -0800

    added assets

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,7 @@ export const siteConfig = {
     email: "danielkimyoyo@gmail.com",
   },
   photos: {
+    title: "/title.gif",
     sky: "https://i.imgur.com/XrKxIyj.jpeg",
     star: "https://i.imgur.com/RIS4sUr.jpeg",
     trail: "https://i.imgur.com/WdldYHn.jpeg",
@@ -17,6 +18,82 @@ export const siteConfig = {
     hop: "https://i.imgur.com/RomTcMD.jpeg",
     seasick: "https://i.imgur.com/DO4nnTJ.jpeg",
     aura: "https://i.imgur.com/Kgc6BVI.jpeg",
+  },
+  gifs: {
+    star: "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3aHZhMzJjaHc5NnU5OWR4bmxsOWN5d243dWdienExdjBsdDJlNDIxOSZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/S6Z6vk4Ex19kb5OjO8/giphy.gif",
+    meteor:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGR0cTJ3aW44dG4wMWJ1dHoyNDF6cDl6emNnaTFsMHdqdDR3cW1meSZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/lTH5TD6XRDo5k6ICzS/giphy.gif",
+    lagann:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcW5kamZlem01eGIwd2Y2cHZhb2RtZHdyMGh4NXUzem9vMGRqeGEwbSZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/nJCXCXBcmbLZQWmjrK/giphy.gif",
+    kamina:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcW5kamZlem01eGIwd2Y2cHZhb2RtZHdyMGh4NXUzem9vMGRqeGEwbSZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/n8SEREo68ttGB0UNb0/giphy.gif",
+    slusny:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmh4d3llbXV5ZTZwdDlzOGZ1YnJ5OGxmcGthaWl5Ymx3aGp2dTVlcyZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/QaGTId0ussJCyVJ0Y6/giphy.gif",
+    becko:
+      "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3MWxyNG5wdnB1aGJhbjUxNnB4Z21tZGdremVwaDdpa2pnMjA4dTc0YiZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/1zkrSr9GCy93arOGbj/giphy.gif",
+    yoyo: "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3bXR3dDNveWpiMXE5YTVzbGdwenNwengyaHB2a2htM3poend5ejVvZCZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/OyXhSEhUunFtnzCmNS/giphy.gif",
+    gentry:
+      "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3MDM2YTBneTQzbWE4cTNiY251eTZtMHVhb2xpYjEwYnRpenN5NmI3cCZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/uUmIX6qVBg1wldcCVO/giphy.gif",
+    neptune:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExajFlcGh6bzd3NjUxMWQyc2FianZ5OHZqbWpicWh6OGZ0ZDdrY3duOCZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/SP2O2JBW2VojK/giphy.gif",
+    mars: "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3bHBsbHYxdzhjYTRoeXM4aG5lYXNwZ3ZhMnNtenhiNnhybDB3dzFkNyZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/ek4CVwa1p9OkS6xhBb/giphy.gif",
+    moon: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExajFlcGh6bzd3NjUxMWQyc2FianZ5OHZqbWpicWh6OGZ0ZDdrY3duOCZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/lIeUAWuLK7cv6/giphy.gif",
+    jupiter:
+      "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3MG53NHFxanVvNmp1M3luam1uN2FiYzgwdTJ0amVwcDV1OWEyOTg5ZiZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/Ytgm5yH5e8yrNByrTV/giphy.gif",
+    magnetic:
+      "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3cTV6dDQzdXg1dXN3YzhlMHQwM2VvNG1tOWpjcG1oOGI3cjM4eWg1NiZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/26BRLnJe1CEsG3xPq/giphy.gif",
+    earth:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOWp3YzdhY3A0engybXNhdDR1MGF3NjUycXduc21tZGQ1dmFuZDI1eiZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/Uy79s1z3cMyCHCMRk1/giphy.gif",
+    saturn:
+      "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3aWpwdXN2N3Q1bGlva292YXVmYmQybm1uMTc1OTEyM3dhNzF5eHMxNiZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/9Pk1N0bFm0pdLLFNQG/giphy.gif",
+    spaceship:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExczZ3ZzV3MnlwYzJhcnZtMXF1bXN6ZmxzNmw0eTE4d25scGVzeGZ6ZyZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/PhCW0WCeQcTixaoouV/giphy.gif",
+    enterprise:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjR1b3BnZ2VsbWZrM2hpOGxqOXl2Z3FkNXh3aGtpOG56bTQwYnA1cSZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/8uWqXQFrt61mE/giphy.gif",
+    borg: "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3YmtnNjhreGpyMWNtcnQwcXRvc2kybjN1cm03dmxrNWp5NGVhanpheCZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/kcBmJjS72a6xdPHQVa/giphy.gif",
+    enterprise2:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdml4ZHAzaWZrcjlyeWh0cWNscWJzY3czNmd6OWZ0amh3emltdjM5MCZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/RgZXfYf92vOec/giphy.gif",
+    star2:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGR0cTJ3aW44dG4wMWJ1dHoyNDF6cDl6emNnaTFsMHdqdDR3cW1meSZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/C4b6GwFKbYxK8/giphy.gif",
+    star3:
+      "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3MnQ5cmU3dGlmaTljZXQ3enRxcjYzZzFiMzkzaXRkMHF1NWYyYTJhMSZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/QI5drz00oA2dOr9tyd/giphy.gif",
+    star4:
+      "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3cHczZjBjZ3Q5M3dwbGVlazl6dWFkZ3IxaDRoeGQ0MDF3enp3bTY3cSZlcD12MV9zdGlja2Vyc19yZWxhdGVkJmN0PXM/yduYHBbbx1Z6Roa0tR/giphy.gif",
+    star5:
+      "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3cHczZjBjZ3Q5M3dwbGVlazl6dWFkZ3IxaDRoeGQ0MDF3enp3bTY3cSZlcD12MV9zdGlja2Vyc19yZWxhdGVkJmN0PXM/ponpgUtBecXaSS03zp/giphy.gif",
+    asteroid:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExaGw3ZTFjN3Uydmt3cDdua3hkdTZ4cDgzNjg1dHQwNmk4OWxzbTlzNCZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/8m54dZsSVWfYUpNcSp/giphy.gif",
+    duel: "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3aTg5OTc2dW5uOWlhazlqcjltM2tjemd1cTB1MzFzOW1yd3JuOHc5dCZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/12Xu8yDLrtJeLu/giphy.gif",
+    sun: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExc2Ntb3B2bnB1MG4zdmgweDY0Zmpwa2I0aXVvMzY0MTJzNnFpN3NzYyZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/y8t1Rj0Dx6kqS1XKVP/giphy.gif",
+    planet:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdXZoNDNudTJuYjd5cjMza3Q1azExdXV4Mmh4azN4bHJocGhjZ2llaSZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/WZq5m5U0ERQMU/giphy.gif",
+    astra:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeW9kdDVkZzdxMWI1cjV4bTQ5NzI4MjF6NDN0dHk4dzQ4azRjcGExZCZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/Z3Es13D8KeZVh7Fczp/giphy.gif",
+    firework:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmQ4ZGlqN3R6M3MyZzZjaGoxdzZ1dXBjbWw2YW15ZzVlZm10dzVldSZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/572Ca3vQAvbcYJW3kL/giphy.gif",
+    boota:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYmM0YXh2YjExcGczbndtZmV4d2QxdHIyNjZ1dzM3c3dwd2o3Z3MzZCZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/q4je6jumXbpDJp70of/giphy.gif",
+    blackhole:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGpmdzk5dGo0dnZ4ZG1zOTM4Z2VxdzZtdDRlaHVrcTNsaHMydTkwbSZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/9iSlGflR0l9987MiFs/giphy.gif",
+    tv: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYXBieW5ueGc2ZXc5dGwzbDlrY2ozbTZsMmZqbDVhbDVtMWhyOGU2cSZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/zQopsKxEZNpErq82to/giphy.gif",
+    falcon:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcTVpMGZzYmFhMjQ2YnE2anFicHhjZWxiZjY2bDd4bTFsZXRtb2pvOSZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/61S8Aj48zWXsbQQpxX/giphy.gif",
+    teletubby:
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYmJveTllbW5qMHRrdzBtemRuODRoa2VqNjVwcWNtbWk0cmNpZnJyNiZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/d4ApxtUS17tQEiAOK8/giphy.gif",
+    eclipse:
+      "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3dDB4c2t2cjl6azljNGp0Nnhoc2puem5pYmpmYW5yaHc5OW9pNnhycCZlcD12MV9zdGlja2Vyc19yZWxhdGVkJmN0PXM/d3RwXTY0jOuDrQMuGl/giphy.gif",
+    eclipse2:
+      "https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3aHY4bnZybW9iZDR6dWUwdWwwbGtvNmtzNjc4cjFtNW9reWFpbWk1MiZlcD12MV9zdGlja2Vyc19yZWxhdGVkJmN0PXM/5Pc2QlK14iRj32Mq8K/giphy.gif",
+    text: "https://3dtext2gif.com/stickers/_3dtext2gif_eagqatx_yaohzvu.gif",
+  },
+
+  backgrounds: {
+    stars:
+      "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExc3RjOHJkbjllM20zdDV6NWFxNHdycGE3OHgxNmk2emxsNzlzOW5ncCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/UYBDCJjwOd9Re/giphy.gif",
+    stars2:
+      "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExeDlneXA1YWwwdWc0N2htdzlwdmxtaWdkNXd3dGJoMzhkZmRnOG9zeSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/FlodpfQUBSp20/giphy.gif",
+    stars3:
+      "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ2ZxMmlyYTZ6Y3FkZXJpZ3ZhMnF5dzhlMHl3YmtwNHhoZnJsenphdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/U3qYN8S0j3bpK/giphy.gif",
   },
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
