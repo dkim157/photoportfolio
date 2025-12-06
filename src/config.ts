6036b87d5026f0b264d7198cbb3f68741b6a77dd
Daniel Kim <danielkimyoyo@gmail.com>
2025-12-06 12:01:33 -0800

    photo testing

--- src/config.ts
+++ src/config.ts
@@ -11,5 +11,8 @@ export const siteConfig = {
     tong: "https://media.giphy.com/media/cYCt3mxdNITTdhjqbm/giphy.gif",
     sakatuca: "https://media.giphy.com/media/Ll7C7CUrphFz783djN/giphy.gif",
     zerb: "https://media.giphy.com/media/XWrz6gbbFYX1WNcvf8/giphy.gif",
+    sky: "/media/sky.JPG",
+    star: "/media/star.JPG",
+    trail: "/media/trail.JPG",
   },
 };
