fcc5148a8ac6886f7968b971c74432eb225e8675
Daniel Kim <danielkimyoyo@gmail.com>
2025-12-05 23:10:16 -0800

    photo page ver 1 done

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   image: {
-    domains: ["i.imgur.com"],
+    domains: ["i.imgur.com", "giphy.com"],
   },
   vite: {
     plugins: [tailwindcss()],
