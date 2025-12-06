e5b08b23ac3f5cb3fe9f17c825298723cc98acf4
Daniel Kim <danielkimyoyo@gmail.com>
2025-12-06 11:45:17 -0800

    minimal branch start

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   image: {
-    domains: ["i.imgur.com", "giphy.com"],
+    domains: ["giphy.com"],
   },
   vite: {
     plugins: [tailwindcss()],
